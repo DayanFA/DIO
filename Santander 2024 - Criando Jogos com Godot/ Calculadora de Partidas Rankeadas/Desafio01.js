814bf912e84bdd338d356c01024742c66f17a8d5
Dayan Freitas Alves <contatodayanfa@gmail.com>
2024-07-11 14:01:00 -0500

    Create Desafio01.js

---
+++ Santander 2024 - Criando Jogos com Godot/ Calculadora de Partidas Rankeadas/Desafio01.js
@@ -0,0 +1,24 @@
+function calcularRank(vitorias, derrotas) {
+    let saldoVitorias = vitorias - derrotas;
+    let nivel;
+
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias >= 10 && vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        nivel = "Lendário";
+    } else if (vitorias >= 101) {
+        nivel = "Imortal";
+    }
+
+    console.log(`O Herói tem de saldo de ${saldoVitorias} está no nível de ${nivel}`);
+}
+
+calcularRank(85, 20); 
