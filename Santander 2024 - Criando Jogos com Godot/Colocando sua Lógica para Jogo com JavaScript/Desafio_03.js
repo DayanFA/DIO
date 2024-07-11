e8314556567e6ef302737ddeeeb92e50ee263f28
Dayan Freitas Alves <contatodayanfa@gmail.com>
2024-07-11 13:48:57 -0500

    Create Desafio_03.js

---
+++ Santander 2024 - Criando Jogos com Godot/Colocando sua Lógica para Jogo com JavaScript/Desafio_03.js
@@ -0,0 +1,21 @@
+// Desafios JavaScript na DIO têm funções "gets" e "print" acessíveis globalmente:
+//- "gets" : lê UMA linha com dado(s) de entrada (inputs) do usuário;
+//- "print" : imprime um texto de saída (output), pulando linha.
+
+const classe = gets();
+
+// TODO: Implemente os demais casos para a resolução do desafio
+switch(classe) {
+  case "Guerreiro":
+      print("Você escolheu a classe Guerreiro!");
+      break;
+  case "Mago":
+      print("Você escolheu a classe Mago!");
+      break;
+  case "Arqueiro":
+      print("Você escolheu a classe Arqueiro!");
+      break;
+  default:
+      print("Classe inválida! Escolha entre Guerreiro, Mago ou Arqueiro.");
+      break;
+}
